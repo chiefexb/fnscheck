83c8b060c8a7fa030bc7cbdbaf7442e98f9540fe
chiefexb <chief_exb@mail.ru>
2015-05-26 19:12:08 +0300

    Second

--- process.py
+++ process.py
@@ -8,6 +8,7 @@ import logging
 import datetime
 import timeit
 import time
+import xlrd
 from odsmod import *
 def getgenerator(cur,gen):
  sq="SELECT GEN_ID("+gen+", 1) FROM RDB$DATABASE"
@@ -36,7 +37,51 @@ class Profiler(object):
 def quoted(a):
  st=u"'"+a+u"'"
  return st
+def inform(st):
+ logging.info(st)
+ print st
+ return
 def main():
+ #лог
+ logging.basicConfig(format = u'%(levelname)-8s [%(asctime)s] %(message)s',level = logging.DEBUG, filename = './process.log')
+ print  len(sys.argv)
+ if len(sys.argv) <2:
+  print "Для запуска набери: ./process.py upload|loadrbd|process|get"
+  print '	loadrbd - Загрузка новых данных из РБД и очистка таблицы от предыдущей версии'
+  print '	process - Поиск соответвий реестров из ГИБДД с данными из РБД'
+  print '       upload     - Загрузка файлов для проверки'
+  print '	get     - Выгрузка реестров для загрузки в подразделениях'
+  sys.exit(2)
+
+ fileconfig=file('./config.xml')
+ xml=etree.parse(fileconfig)
+ xmlroot=xml.getroot()
+ nd=xmlroot.find('input_path')
+ input_path=nd.text
+
+ nd=xmlroot.find('input_arc_path')
+ input_arc_path=nd.text
+
+ main_database=xmlroot.find('main_database')
+ main_dbname=main_database.find('dbname').text
+ main_user=main_database.find('user').text
+ main_password=main_database.find('password').text
+ main_host=main_database.find('hostname').text
+
+# rbd_database=xmlroot.find('rbd')
+# rbd_dbname=rbd_database.find('dbname').text
+# rbd_user=rbd_database.find('user').text
+# rbd_password=rbd_database.find('password').text
+# rbd_host=rbd_database.find('hostname').text
  st=''
+ print input_path
+ if sys.argv[1]=='upload':
+  for ff in listdir(input_path):
+   print ff
+   #открыть файл
+   wb=xlrd.book.open_workbook_xls(input_path+ff)
+   ws=wb.sheet_by_index(0)
+   print ws.cell_value(2,0)
+  
 if __name__ == "__main__":
     main()
